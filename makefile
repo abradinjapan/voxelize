12f86eeefd9038f758b801418206052c018b90a2
abradinjapan <abradinjapan@outlook.com>
2024-01-09 21:06:55 -0500

    Working on world manager. Removed binary folder.

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 debug:
-	gcc source/main.c -Wextra -Wall -fsanitize=address -o binary/voxelize_debug -g -lSDL2 -lGL -lGLEW -lm
+	gcc source/main.c -Wextra -Wall -fsanitize=address -o ../voxelize.elf -g -lSDL2 -lGL -lGLEW -lm
 
 release:
-	gcc source/main.c -Wextra -Wall -o binary/voxelize_release -g -lSDL2 -lGL -lGLEW -lm
+	gcc source/main.c -Wextra -Wall -o ../voxelize.elf -g -lSDL2 -lGL -lGLEW -lm
--- source/managing/world_manager.h
+++ source/managing/world_manager.h
@@ -1,6 +1,11 @@
 #ifndef VOXELIZE__world_manager
 #define VOXELIZE__world_manager
 
+/* Notes */
+/*
+    The world must always be rendered with the check of "is this chunk drawn?"
+*/
+
 /* Include */
 // voxelize
 #include "../rendering/render.h"
@@ -14,10 +19,10 @@ typedef CHUNK__chunks_index MANAGER__chunks_index;
 typedef RENDER__object_index MANAGER__rendering_object_index;
 
 // invalid index
-#define POS__define__invalid_index -1
+#define MANAGER__define__invalid_index -1
 
-/* Manager */
-// one index into the chunk manager
+/* Slots */
+// one slot of a chunk (body & array)
 typedef struct MANAGER__chunk_body_slot {
     // open spot flag
     BASIC__bt p_slot_open;
@@ -27,6 +32,51 @@ typedef struct MANAGER__chunk_body_slot {
     MANAGER__rendering_object_index p_rendering_index;
 } MANAGER__chunk_body_slot;
 
+// create custom body slot
+MANAGER__chunk_body_slot MANAGER__create__chunk_body_slot(BASIC__bt slot_open, MANAGER__chunks_index chunks_index, MANAGER__rendering_object_index rendering_index) {
+    MANAGER__chunk_body_slot output;
+
+    // setup output
+    output.p_slot_open = slot_open;
+    output.p_chunks_index = chunks_index;
+    output.p_rendering_index = rendering_index;
+
+    return output;
+}
+
+// create bull body slot
+MANAGER__chunk_body_slot MANAGER__create_null__chunk_body_slot() {
+    // return empty
+    return MANAGER__create__chunk_body_slot(BASIC__bt__false, MANAGER__define__invalid_index, MANAGER__define__invalid_index);
+}
+
+// one slot of a chunk (surface)
+typedef struct MANAGER__chunk_surface_slot {
+    // open spot flag
+    BASIC__bt p_slot_open;
+
+    // chunk array & chunk body indices
+    MANAGER__rendering_object_index p_rendering_index;
+} MANAGER__chunk_surface_slot;
+
+// create custom surface slot
+MANAGER__chunk_surface_slot MANAGER__create__chunk_surface_slot(BASIC__bt slot_open, MANAGER__rendering_object_index rendering_index) {
+    MANAGER__chunk_surface_slot output;
+
+    // setup output
+    output.p_slot_open = slot_open;
+    output.p_rendering_index = rendering_index;
+
+    return output;
+}
+
+// create null surface slot
+MANAGER__chunk_surface_slot MANAGER__create_null__chunk_surface_slot() {
+    // return empty
+    return MANAGER__create__chunk_surface_slot(BASIC__bt__false, MANAGER__define__invalid_index);
+}
+
+/* Manager */
 // manager type
 typedef struct MANAGER__world_manager {
     // tracked objects
@@ -41,5 +91,10 @@ typedef struct MANAGER__world_manager {
     BASIC__buffer p_chunk_body_slots;
     BASIC__buffer p_chunk_surface_slots;
 } MANAGER__world_manager;
+
+// open a world manager
+MANAGER__world_manager MANAGER__open__world_manager() {
+    
+}
 
 #endif
